06cf9d94836fa4c9dd44edd1a09f3e5777beae93
xieyy <xieyy@xieyy.toone.com.cn>
2020-01-15 16:13:52 +0800

    2

--- src/test/java/cn/xieyy/utils/webdata/Demo5.java
+++ src/test/java/cn/xieyy/utils/webdata/Demo5.java
@@ -10,6 +10,7 @@ import java.util.Set;
 public class Demo5 {
     public static void main(String args[]) throws Exception {
       System.out.println("1");
+      System.out.println("2");
     }
 
 
