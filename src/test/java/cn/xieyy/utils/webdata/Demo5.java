f5eab7ed4afd289c7e2fd79e548d9c651c0dcf5b
xieyy <xieyy@xieyy.toone.com.cn>
2020-01-15 16:11:38 +0800

    1

---
+++ src/test/java/cn/xieyy/utils/webdata/Demo5.java
@@ -0,0 +1,17 @@
+package cn.xieyy.utils.webdata;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import java.util.List;
+import java.util.Set;
+
+public class Demo5 {
+    public static void main(String args[]) throws Exception {
+      System.out.println("1");
+    }
+
+
+}
+
